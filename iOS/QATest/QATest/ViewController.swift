dc74064e6a052cf997053d37da63ce1fc59e771c
Simon Collins <simon@twobulls.com>
2018-10-29 13:52:16 +1100

    Add iOS testing project.

---
+++ iOS/QATest/QATest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QATest
+//
+//  Created by Chris Taranto on 15/10/18.
+//  Copyright © 2018 Two Bulls. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ iOS/QATest/QATestTests/QATestTests.swift
@@ -0,0 +1,34 @@
+//
+//  QATestTests.swift
+//  QATestTests
+//
+//  Created by Chris Taranto on 15/10/18.
+//  Copyright © 2018 Two Bulls. All rights reserved.
+//
+
+import XCTest
+@testable import QATest
+
+class QATestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
